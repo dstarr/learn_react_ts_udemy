b3d31fa532f8ff2f5b21edc6b4761e90e333c6e7
David Starr <david@elegantcode.com>
2025-02-23 18:17:38 -0800

    safety commit

---
+++ lesson_3/begin-optional-props/src/App.tsx
@@ -0,0 +1,11 @@
+import TitleComponent from "./components/TitleComponent"
+
+const App = () => {
+    return (
+        <div className="flex flex-col h-screen justify-center items-center">
+            <TitleComponent textColor="text-purple-500" />   
+        </div>
+    )
+}
+
+export default App
---
+++ lesson_3/begin-optional-props/src/components/TitleComponent.tsx
@@ -0,0 +1,9 @@
+interface TitleComponentProps {
+    textColor?: 'text-blue-500' | 'text-red-500' | 'text-green-500' | 'text-yellow-500' | 'text-purple-500'
+}
+
+const TitleComponent = ({ textColor = 'text-blue-500' }: TitleComponentProps) => {
+    return <h1 className={`font-bold text-4xl ${textColor}`}>React + TypeScript</h1>
+}
+
+export default TitleComponent
