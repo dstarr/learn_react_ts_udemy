2f2096553db8c79842a1df7cee6c068c1d90c986
David Starr <david@elegantcode.com>
2025-03-25 17:37:35 -0700

    safety commit

--- 18_journel_app/working/src/components/AddJournalForm.tsx
+++ 18_journel_app/working/src/components/AddJournalForm.tsx
@@ -15,7 +15,13 @@ const formSchema = z.object({
 type FormData = z.infer<typeof formSchema>;
 
 const AddJournalForm = () => {
-    
+    const openDialog = () => {
+        const modal = document.getElementById('success_modal_dialog');
+        if (modal instanceof HTMLDialogElement) {
+            modal.showModal();
+        }
+    };
+
     const {
         register,
         handleSubmit,
@@ -26,14 +32,58 @@ const AddJournalForm = () => {
     });
 
     const onFormSubmit = (data: FieldValues) => {
-        console.log(data);
+        // create an id
+        const id = crypto.randomUUID();
+        data.id = id;
+
+        // timestamp
+        data.createdAt = new Date().toLocaleString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+
+        // retrieve from local storage
+        const journalEntries = JSON.parse(
+            localStorage.getItem('journals') || '[]'
+        );
+
+        // save to local storage
+        journalEntries.push(data);
+        localStorage.setItem('journals', JSON.stringify(journalEntries));
+
+        openDialog();
+
+        // reset the form with default values
+        reset({
+            title: '',
+            feeling: 'Happy',
+            content: ''
+        });
     };
-
-
 
     return (
         <>
-            <form className="flex flex-col gap-3" onSubmit={handleSubmit(onFormSubmit)}>
+            <dialog id="success_modal_dialog" className="modal modal-middle">
+                <div id="success_modal" className="modal-box">
+                    <h3 className="font-bold">Journal saved successfully!</h3>
+                    <p className="py-4">
+                        Press ESC key or click the button below to close
+                    </p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-primary">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+
+            <form
+                className="flex flex-col gap-3"
+                onSubmit={handleSubmit(onFormSubmit)}
+            >
+
                 <div className="flex flex-col gap-2">
                     <label
                         className="input input-bordered flex w-full items-center gap-2"
@@ -44,6 +94,7 @@ const AddJournalForm = () => {
                             type="text"
                             className="grow"
                             placeholder="Title"
+                            {...register('title')}
                         />
                     </label>
                     {errors.title && (
@@ -56,7 +107,7 @@ const AddJournalForm = () => {
                     </label>
                     <select
                         id="feeling"
-                        defaultValue="Pick a color"
+                        defaultValue="Happy"
                         className="select select-bordered w-full"
                         {...register('feeling')}
                     >
@@ -74,7 +125,9 @@ const AddJournalForm = () => {
                             {errors.content.message}
                         </p>
                     )}
-                    <button className="btn btn-primary" type="submit">Save</button>
+                    <button className="btn btn-primary" type="submit">
+                        Save
+                    </button>
                 </div>
             </form>
         </>
